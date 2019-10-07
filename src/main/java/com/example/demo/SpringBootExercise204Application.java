56b5bce8cafce958538b2dbd3f88ca27b124a714
yusufr98 <yusuf090798@gmail.com>
2019-10-07 15:42:02 -0400

    initial commit

---
+++ src/main/java/com/example/demo/SpringBootExercise204Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExercise204Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExercise204Application.class, args);
+    }
+
+}
